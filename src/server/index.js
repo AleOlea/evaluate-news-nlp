31eb7354f577feac82d932af23555e87c47b57c0
AleOlea <espiritusdelfuego@gmail.com>
2021-04-14 15:31:35 -0400

    Installing jest

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 var path = require("path");
 const express = require("express");
